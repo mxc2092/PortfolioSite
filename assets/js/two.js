00892095045361f5f2d7a0e6b7bb15feadf2959a
mxc2092 <54378191+mxc2092@users.noreply.github.com>
2020-10-14 08:23:03 -0400

    added art
    art

--- assets/js/two.js
+++ assets/js/two.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 $("#twoPic1").mouseover(function(){
 
   $("#twoPic1").css("background-image", "url('./assets/images/dc.jpg')");
-}); 
+});
 
 $("#twoPic1").mouseleave(function(){
 
@@ -16,7 +16,7 @@ $("#twoPic1").mouseleave(function(){
 $("#twoPic5").mouseover(function(){
 
 	$("#twoPic5").css("background-image", "url('./assets/images/fair3.JPG')");
-});	
+});
 
 $("#twoPic5").mouseleave(function(){
 
@@ -27,11 +27,22 @@ $("#twoPic5").mouseleave(function(){
 $("#twoPic4").mouseover(function(){
 
   $("#twoPic4").css("background-image", "url('./assets/images/design22.JPG')");
-}); 
+});
 
 $("#twoPic4").mouseleave(function(){
 
   $("#twoPic4").css("background-image", "url('./assets/images/design4.png')");
+
+});
+
+$("#twoPic11").mouseover(function(){
+
+  $("#twoPic11").css("background-image", "url('./assets/images/murphysketch.png')");
+});
+
+$("#twoPic11").mouseleave(function(){
+
+  $("#twoPic11").css("background-image", "url('./assets/images/murphysketch1.png')");
 
 });
 
